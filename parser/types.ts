6c5c3bafd88198f3f40dd05834eb2834b9f92f77
tsukiroku <tsukiroku@yahoo.com>
2022-06-30 17:43:16 +0900

    Update parser

---
+++ parser/types.ts
@@ -0,0 +1,131 @@
+import { TokenType } from '../tokenizer';
+
+interface Program {
+    statements: Array<Statement>;
+}
+
+type Statement = LetStatement | ReturnStatement | ExpressionStatement;
+
+type Expression =
+    | LiteralExpression
+    | BlockStatement
+    | PrefixExpression
+    | InfixExpression
+    | IfExpression
+    | FunctionExpression
+    | CallExpression
+    | IdentExpression
+    | ArrayExpression
+    | IndexExpression
+    | HashExpression;
+
+interface LetStatement {
+    ident: Expression;
+    value: Expression;
+}
+
+interface ReturnStatement {
+    value: Expression;
+}
+
+interface ExpressionStatement {
+    expression: Expression;
+}
+
+interface LiteralExpression {
+    value: NumberLiteral | StringLiteral | BooleanLiteral | UnitLiteral;
+}
+
+interface BlockStatement {
+    statements: Array<Statement>;
+}
+
+interface PrefixExpression {
+    operator: TokenType;
+    right: Expression;
+}
+
+interface InfixExpression {
+    left: Expression;
+    right: Expression;
+    operator: TokenType;
+}
+
+interface IfExpression {
+    condition: Expression;
+    consequence: Expression;
+    alternative?: Expression;
+}
+
+interface FunctionExpression {
+    arguments: Array<Expression>;
+    body: Statement;
+}
+
+interface CallExpression {
+    function: Expression;
+    arguments: Array<Expression>;
+}
+
+interface ArrayExpression {
+    elements: Array<Expression>;
+}
+
+interface IndexExpression {
+    left: Expression;
+    index: Expression;
+}
+
+interface IdentExpression {
+    value: string;
+}
+
+interface HashExpression {
+    pairs: Array<HashPair>;
+}
+
+interface HashPair {
+    key: Expression;
+    value: Expression;
+}
+
+interface NumberLiteral {
+    value: number;
+}
+
+interface StringLiteral {
+    value: string;
+}
+
+interface BooleanLiteral {
+    value: boolean;
+}
+
+interface UnitLiteral {
+    value: null;
+}
+
+export {
+    Program,
+    Statement,
+    Expression,
+    LetStatement,
+    ReturnStatement,
+    ExpressionStatement,
+    LiteralExpression,
+    BlockStatement,
+    PrefixExpression,
+    InfixExpression,
+    IfExpression,
+    FunctionExpression,
+    CallExpression,
+    ArrayExpression,
+    IndexExpression,
+    IdentExpression,
+    HashExpression,
+    HashPair,
+    NumberLiteral,
+    StringLiteral,
+    BooleanLiteral,
+    UnitLiteral,
+};
