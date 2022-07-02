965812c89ac1cdc2a18a0e8cba46c5d9becfe3ad
tsukiroku <tsukiroku@yahoo.com>
2022-07-02 16:31:30 +0900

    Update Object

--- object/index.ts
+++ object/index.ts
@@ -2,14 +2,4 @@ import Enviroment from './env';
 
 export { Enviroment };
 
-export {
-    LangObject,
-    NumberObject,
-    BooleanObject,
-    StringObject,
-    ArrayObject,
-    HashObject,
-    FunctionObject,
-    BuiltinFunction,
-    QuoteObject,
-} from './object';
+export * from './object';
--- object/object.ts
+++ object/object.ts
@@ -10,40 +10,74 @@ type LangObject =
     | FunctionObject
     | BuiltinFunction
     | QuoteObject
+    | ReturnValue
+    | ErrorObject
     | null;
+
+enum ObjectKind {
+    NUMBER,
+    STRING,
+    BOOLEAN,
+    ARRAY,
+    HASH,
+    FUNCTION,
+    BUILTIN,
+    QUOTE,
+    RETURN_VALUE,
+    ERROR,
+    NULL,
+}
 
 interface NumberObject {
     value: number;
+    kind: ObjectKind.NUMBER;
 }
 
 interface BooleanObject {
     value: boolean;
+    kind: ObjectKind.BOOLEAN;
 }
 
 interface StringObject {
     value: string;
+    kind: ObjectKind.STRING;
 }
 
 interface ArrayObject {
     value: Array<LangObject>;
+    kind: ObjectKind.ARRAY;
 }
 
 interface HashObject {
     pairs: Map<NumberObject | BooleanObject | StringObject, LangObject>;
+    kind: ObjectKind.HASH;
 }
 
 interface FunctionObject {
     parameters: Array<Expression>;
     body: Expression;
     env: Enviroment;
+    kind: ObjectKind.FUNCTION;
 }
 
 interface BuiltinFunction {
     (...args: Array<LangObject>): LangObject;
+    kind: ObjectKind.BUILTIN;
 }
 
 interface QuoteObject {
     value: Expression;
+    kind: ObjectKind.QUOTE;
+}
+
+interface ReturnValue {
+    value: LangObject;
+    kind: ObjectKind.RETURN_VALUE;
+}
+
+interface ErrorObject {
+    message: string;
+    kind: ObjectKind.ERROR;
 }
 
 export {
@@ -56,4 +90,7 @@ export {
     FunctionObject,
     BuiltinFunction,
     QuoteObject,
+    ReturnValue,
+    ErrorObject,
+    ObjectKind,
 };
