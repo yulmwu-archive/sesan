0514331ce6eacb1176706d4ba07b5c04a2677b7e
tsukiroku <tsukiroku@yahoo.com>
2022-07-02 12:57:40 +0900

    Update Parser

--- parser/index.ts
+++ parser/index.ts
@@ -1,3 +1,27 @@
 import Parser from './parser';
 
 export { Parser };
+
+export {
+    Program,
+    Statement,
+    Expression,
+    LetStatement,
+    ReturnStatement,
+    ExpressionStatement,
+    LiteralExpression,
+    BlockStatement,
+    PrefixExpression,
+    InfixExpression,
+    IfExpression,
+    FunctionExpression,
+    CallExpression,
+    ArrayExpression,
+    IndexExpression,
+    IdentExpression,
+    HashExpression,
+    HashPair,
+    NumberLiteral,
+    StringLiteral,
+    BooleanLiteral,
+} from './types';
