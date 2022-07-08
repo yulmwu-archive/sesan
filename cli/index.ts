dc4975c4167a537dbcfc2b590a42000878479c0e
tsukiroku <tsukiroku@yahoo.com>
2022-07-08 12:38:52 +0900

    Update CLI

---
+++ cli/index.ts
@@ -0,0 +1,33 @@
+import { existsSync, readFileSync } from 'fs';
+import { evaluator, printError } from '../evaluator';
+import { Enviroment, ObjectKind } from '../object';
+import parseOptions from '../options';
+import { Parser } from '../parser';
+import { Lexer } from '../tokenizer';
+import Repl from './repl';
+
+const args = process.argv.slice(2);
+
+const env = new Enviroment();
+
+const option = existsSync('./tiny.config.json')
+    ? parseOptions(readFileSync('./tiny.config.json').toString())
+    : parseOptions();
+
+if (args.length <= 0) new Repl(env, option).start();
+else {
+    try {
+        const file = readFileSync(args[0], 'utf8');
+
+        const parser = new Parser(new Lexer(file));
+
+        const result = evaluator(parser.parseProgram(), env, option);
+
+        if (parser.errors.length > 0)
+            parser.errors.forEach((error) => printError(error));
+
+        if (result?.kind === ObjectKind.ERROR) printError(result.message);
+    } catch (e) {
+        console.error('Cannot open file: ' + args[0]);
+    }
+}
---
+++ cli/repl.ts
@@ -0,0 +1,131 @@
+import prompt from 'prompt-sync';
+import colors from 'colors';
+
+import { evaluator, printError } from '../evaluator';
+import { Enviroment, LangObject, objectStringify, ObjectKind } from '../object';
+import { Parser, Program } from '../parser';
+import { Lexer, Token, TokenType } from '../tokenizer';
+import { Options } from '../options';
+
+type Mode = 'repl' | 'parser' | 'parser_Json' | 'lexer' | 'env';
+
+export default class {
+    public promptSync = prompt({ sigint: true });
+    public mode: Mode = 'repl';
+
+    constructor(public env: Enviroment, public option: Options) {
+        colors.enabled = true;
+    }
+
+    public executeCommand(
+        input: string,
+        lexer: Lexer,
+        parsed: Program,
+        env: Enviroment
+    ): LangObject | Program | Array<Token> | Enviroment | string {
+        const [command, ...args] = input.split(' ');
+        const commands: Map<
+            string,
+            (...args: Array<string>) => LangObject | string
+        > = new Map([
+            [
+                '#mode',
+                (...args) => {
+                    if (
+                        !['repl', 'parser', 'lexer', 'env'].includes(args[0]) ||
+                        args.length <= 0
+                    )
+                        return 'Invalid mode. valid modes are `repl`, `parser`, `parser json`, `lexer`, and `env`';
+                    if (
+                        args[0] === 'parser' &&
+                        (args.length >= 2 && args[1].toLowerCase()) === 'json'
+                    )
+                        this.mode = 'parser_Json';
+                    else this.mode = args[0] as Mode;
+
+                    return `Switched to '${this.mode}' mode`;
+                },
+            ],
+            [
+                '#import',
+                () => {
+                    evaluator(
+                        new Parser(
+                            new Lexer(`import('@std/lib');`)
+                        ).parseProgram(),
+                        env,
+                        this.option
+                    );
+
+                    return 'Imported std';
+                },
+            ],
+            ['#exit', () => process.exit(0)],
+        ]);
+
+        if (commands.has(command)) return commands.get(command)!(...args);
+
+        const result = evaluator(parsed, env, this.option);
+
+        if (result?.kind === ObjectKind.ERROR) {
+            printError(result.message);
+            return '';
+        }
+
+        switch (this.mode) {
+            case 'repl':
+                return objectStringify(result).gray;
+
+            case 'parser':
+                return parsed;
+
+            case 'parser_Json':
+                return JSON.stringify(parsed, null, 2);
+
+            case 'lexer':
+                const tokens: Array<Token> = [];
+
+                let peekToken: Token;
+
+                while ((peekToken = lexer.nextToken()).type !== TokenType.EOF)
+                    tokens.push(peekToken);
+
+                return tokens;
+
+            case 'env':
+                return env;
+        }
+    }
+
+    public start() {
+        if (this.option.useStdLibAutomatically)
+            evaluator(
+                new Parser(new Lexer(`import('@std/lib');`)).parseProgram(),
+                this.env,
+                this.option
+            );
+
+        while (true) {
+            const input = this.promptSync(
+                `${`[${this.mode.toUpperCase()}]`.white.bgBlack} ${
+                    `${this.env.store.size} Env(s)`.gray
+                } ${'➜'.red} `
+            );
+
+            const parser = new Parser(new Lexer(input));
+
+            const executed = this.executeCommand(
+                input,
+                new Lexer(input),
+                parser.parseProgram(),
+                this.env
+            );
+
+            if (executed)
+                if (parser.errors.length > 0)
+                    parser.errors.forEach((error) => printError(error));
+
+            console.log(executed, '\n');
+        }
+    }
+}
