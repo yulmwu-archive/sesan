fe00119745c02f4d5a372e86000172288157d906
tsukiroku <tsukiroku@yahoo.com>
2022-07-09 13:04:35 +0900

    Update CLI

--- cli/index.ts
+++ cli/index.ts
@@ -1,10 +1,8 @@
 import { existsSync, readFileSync } from 'fs';
-import { Evaluator, printError } from '../evaluator';
-import { Enviroment, ObjectKind } from '../object';
+import { Enviroment } from '../object';
 import parseOptions from '../options';
-import { Parser } from '../parser';
-import { Lexer } from '../tokenizer';
 import Repl from './repl';
+import Tiny from '..';
 
 const args = process.argv.slice(2);
 
@@ -19,14 +17,11 @@ else {
     try {
         const file = readFileSync(args[0], 'utf8');
 
-        const parser = new Parser(new Lexer(file));
-
-        const result = new Evaluator(parser.parseProgram(), env, option).eval();
-
-        if (parser.errors.length > 0)
-            parser.errors.forEach((error) => printError(error));
-
-        if (result?.kind === ObjectKind.ERROR) printError(result.message);
+        new Tiny(file, {
+            // @ts-ignore
+            enviroment: env,
+            ...option,
+        }).eval();
     } catch (e) {
         console.error('Cannot open file: ' + args[0]);
     }
