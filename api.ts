a8d35ba7c7987fbfd6d1fef9ea4051887c547072
tsukiroku <tsukiroku@yahoo.com>
2022-07-14 22:01:14 +0900

    Update

---
+++ api.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import Tiny from '.';
+import { NULL } from './evaluator';
+
+const app = express();
+
+app.listen(5050, () => console.log('http://localhost:5050'));
+
+app.get('/eval/:code', (req, res) => {
+    const result: Array<string> = [];
+
+    new Tiny(req.params.code, {
+        useStdLibAutomatically: true,
+        root: './',
+    })
+        .applyBuiltins()
+        .setStdout((x) => result.push(x))
+        .setStdin(() => NULL)
+        .eval();
+
+    res.json({
+        result,
+    });
+});
--- cli/index.ts
+++ cli/index.ts
@@ -1,9 +1,8 @@
 import { existsSync, readFileSync } from 'fs';
 import { Enviroment } from '../object';
-import Tiny, { NULL } from '../index';
+import Tiny from '../index';
 import parseOptions from '../options';
 import Repl from './repl';
-import express from 'express';
 
 const args = process.argv.slice(2);
 
@@ -14,28 +13,7 @@ const option = existsSync('./tiny.config.json')
     : parseOptions();
 
 if (args.length <= 0) new Repl(env, option).start();
-else if (args[0] === '--api') {
-    const app = express();
-
-    app.listen(5050, () => console.log('http://localhost:5050'));
-
-    app.get('/eval/:code', (req, res) => {
-        const result: Array<string> = [];
-
-        new Tiny(req.params.code, {
-            useStdLibAutomatically: true,
-            root: './',
-        })
-            .applyBuiltins()
-            .setStdout((x) => result.push(x))
-            .setStdin(() => NULL)
-            .eval();
-
-        res.json({
-            result,
-        });
-    });
-} else {
+else {
     try {
         const file = readFileSync(args[0], 'utf8');
 
