b2d3671ccf332a13cb445bbc1788f2f370a6c652
tsukiroku <tsukiroku@yahoo.com>
2022-07-14 21:34:46 +0900

    Update

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { Enviroment, ObjectKind, objectStringify } from './object';
 import { Options } from './options';
 import { Parser } from './parser';
 import { Lexer } from './tokenizer';
+import prompt from 'prompt-sync';
 
 export * from './object';
 export * from './options';
@@ -11,43 +12,58 @@ export * from './tokenizer';
 export * from './parser';
 export * from './evaluator';
 
+type Stdio = (...x: Array<any>) => any;
+
 type TinyOption = Options & {
     enviroment?: Enviroment;
+    root?: string;
 };
+
+interface StdioOptions {
+    stdin: Stdio;
+    stdout: Stdio;
+}
+
+const stdin: Stdio = (...x) => prompt({ sigint: true });
+const stdout: Stdio = (...x) => process.stdout.write(x.join(' '));
 
 export default class Tiny {
     public option: TinyOption;
     public builtins: Map<string, Func> = new Map();
+    public stdio: StdioOptions = { stdin, stdout };
 
     constructor(public x: string, option?: TinyOption) {
         this.option = { ...option };
     }
 
     public tokenizer(): Lexer {
-        return new Lexer(this.x);
+        return new Lexer(this.x, this.stdio.stdout);
     }
 
     public eval(): string {
         const env = this.option.enviroment ?? new Enviroment();
-        const parser = new Parser(this.tokenizer());
 
         if (this.option.useStdLibAutomatically)
             new Evaluator(
-                new Parser(new Lexer(`import('@std/lib');`)).parseProgram(),
+                new Parser(
+                    new Lexer(`import('@std/lib');`, this.stdio.stdin)
+                ).parseProgram(),
                 env,
-                this.option
+                this.option,
+                this.stdio,
+                this.option.root
             ).eval();
 
         const result = new Evaluator(
-            parser.parseProgram(),
+            new Parser(this.tokenizer()).parseProgram(),
             env,
-            this.option
+            this.option,
+            this.stdio,
+            this.option.root
         ).eval();
 
-        if (parser.errors.length > 0)
-            parser.errors.forEach((error) => printError(error));
-
-        if (result?.kind === ObjectKind.ERROR) printError(result.message);
+        if (result?.kind === ObjectKind.ERROR)
+            printError(result.message, this.stdio.stdout);
 
         return objectStringify(result);
     }
@@ -69,6 +85,18 @@ export default class Tiny {
 
         return this;
     }
+
+    public setStdin(func: Stdio): Tiny {
+        this.stdio = { ...this.stdio, stdin: func };
+
+        return this;
+    }
+
+    public setStdout(func: Stdio): Tiny {
+        this.stdio = { ...this.stdio, stdout: func };
+
+        return this;
+    }
 }
 
-export { TinyOption };
+export { TinyOption, Stdio, StdioOptions, stdin, stdout };
