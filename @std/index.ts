87d6e73d2aa5cac5bb90fae77ecefcc7ae875e2f
tsukiroku <tsukiroku@yahoo.com>
2022-07-15 21:43:44 +0900

    Update

--- @std/index.ts
+++ @std/index.ts
@@ -2,7 +2,7 @@ import { readdirSync, readFileSync } from 'fs';
 
 export default readdirSync('./').filter((x) => x.endsWith('.tiny'));
 
-const content = (x: string): string => readFileSync(`./${x}.tiny`, 'utf8');
+const content = (x: string): string => readFileSync(`./@std/${x}.tiny`, 'utf8');
 
 export const stds = {
     'io.ts': content('io'),
