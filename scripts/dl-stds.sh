a6a2db2d8b09a2f705934b786943981642eb3283
tsukiroku <tsukiroku@yahoo.com>
2022-08-23 20:28:00 +0900

    Update Scripts

---
+++ scripts/dl-stds.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FILES=("array.tiny" "io.tiny" "lib.tiny" "string.tiny" "util.tiny")
+
+STD_PATH="./@stds"
+
+if [ ! -d $STD_PATH ]; then
+    if [ ! -d STD_PATH ]; then
+        mkdir $STD_PATH
+    fi
+
+    START_TIME=$SECONDS
+
+    echo "Downloading standard library... ($STD_PATH)"
+
+    for file in ${FILES[@]}; do
+        curl -o $STD_PATH/$file https://raw.githubusercontent.com/tsukiroku/tiny/main/%40std/$file >/dev/null 2>&1
+        echo "Downloading $file"
+    done
+
+    echo "Done ($((SECONDS - START_TIME))s) / ${#FILES[@]} Files.\n"
+else
+    echo "Directory $STD_PATH is already exists"
+fi
